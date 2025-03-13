3c2a72cb11f3053ffa679d0244d9905e21788caa
ateeb <mateeb.ce41ceme@ce.ceme.edu.pk>
2025-03-14 03:23:49 +0500

    Initial commit for Render deployment

--- app2.py
+++ app2.py
@@ -1031,5 +1031,7 @@ def initialize_database():
     db.create_all()
     create_initial_data()
 
+# Add this near the end of your app.py file
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5001)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
